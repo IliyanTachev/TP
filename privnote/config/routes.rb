8b8cc6f937d2b4f9c5d80a73b7d9c43a890217da
RobertaNetzova123 <roberta.netzova@fmail.com>
2017-11-05 15:29:36 -0800

    Ludiat chovek znae ne che e lud az sum lud sledovatelno ne sum lud...

--- privnote/config/routes.rb
+++ privnote/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
- 	#get 'messages/new'
+	
 	root 'messages#new'
+  
 	resources :messages
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	post "notes/api" => "messages#create"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
